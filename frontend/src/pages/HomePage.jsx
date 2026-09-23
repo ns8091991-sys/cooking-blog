95c748ccb3a982b237b15183e80805aedb669bb1
анастасия <fytguhjk@dfghj.ruil.com>
2026-09-23 21:00:39 +0300

    Fix gitignore, remove env/pycache, add hero image

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -35,7 +35,7 @@ export default function HomePage() {
             </div>
           </div>
           <div className="hero__media">
-            <img src="/photho.jpg" alt="..." />
+            <img src="/photho.jpg" alt="" />
           </div>
 
         </div>
